c1828f4b03582185b88e7e20a156d35478ffa410
Dima Nemets <dimanemets@gmail.com>
2024-06-10 09:44:20 +0300

    Update stocks_assingment.py

--- jobs/stocks_assingment.py
+++ jobs/stocks_assingment.py
@@ -28,7 +28,7 @@ s3_output_path = args['S3_OUTPUT_PATH']
 # the code
 TRADING_DAYS = 252
 
-df = spark.read.csv(s3_input_path, header=True, inferSchema=True)
+df = spark.read.csv(s3_input_path, header=True, inferSchema=True, dateFormat="M/d/yyyy").select("date", "close", "volume", "ticker")
 
 window_spec = Window.partitionBy("ticker").orderBy("date")
 
@@ -37,28 +37,30 @@ data_close_price = df.withColumn("prev_close", lag("close").over(window_spec))
 data_daily_returns = data_close_price.withColumn("daily_return", ((col("close") - col("prev_close")) / col("prev_close")) * 100)
 # caching daily returns because it's being used multiple times
 data_daily_returns = data_daily_returns.filter(col("daily_return").isNotNull()).cache()
-average_daily_return = data_daily_returns.groupBy("date").agg(avg("daily_return").alias("average_daily_return"))
-average_daily_return.coalesce(1).write.csv(f"{s3_output_path}/01-average_daily_return", header=True, mode="overwrite")
+average_daily_return = data_daily_returns.groupBy("date").agg(avg("daily_return").alias("average_return"))
+average_daily_return.show(10, False)
+average_daily_return.write.mode("overwrite").parquet(f"{s3_output_path}/average_daily_return")
 
 # avg frequency
 frequency = df.withColumn("frequency", col("close") * col("volume"))
-average_frequency = frequency.groupBy("ticker").agg(avg("frequency").alias("average_frequency"))
-average_frequency.coalesce(1).write.csv(f"{s3_output_path}/02-average_frequency", header=True, mode="overwrite")
+average_frequency = frequency.groupBy("ticker").agg(avg("frequency").alias("frequency"))
+average_frequency.show(10, False)
+average_frequency.write.mode("overwrite").parquet(f"{s3_output_path}/average_frequency")
 
 # most volatile stocks
 volatility = data_daily_returns.groupBy("ticker").agg((stddev("daily_return") * sqrt(TRADING_DAYS)).alias("standard_deviation"))
-volatility.coalesce(1).write.csv(f"{s3_output_path}/03-volatility", header=True, mode="overwrite")
+volatility.show(10, False)
+volatility.write.mode("overwrite").parquet(f"{s3_output_path}/volatility")
 
 # top 30 day return dates
 top_30_day_return_dates = (
         df.withColumn("prev_30_day_close", lag("close", 30).over(window_spec))
         .withColumn("30_day_return", ((col("close") - col("prev_30_day_close")) / col("prev_30_day_close")) * 100)
         .filter(col("30_day_return").isNotNull())
-        .withColumn("row_number", row_number().over(Window.partitionBy("ticker").orderBy(col("30_day_return").desc())))
-        .filter(col("row_number") <= 3)
         .select("ticker", "date", "30_day_return")
+        .orderBy(col("30_day_return").desc())
 )
-top_30_day_return_dates.coalesce(1).write.csv(f"{s3_output_path}/04-top_30_day_return_dates", header=True, mode="overwrite")
-
+top_30_day_return_dates.write.mode("overwrite").parquet(f"{s3_output_path}/top_30_day_return_dates")
+top_30_day_return_dates.limit(3).show(3, False)
 
 job.commit()
